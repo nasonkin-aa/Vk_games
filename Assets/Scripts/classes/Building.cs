a557d2dfd78494da26cf87426fe3b83f4b22c9a2
nasonkin-aa <game-akaynt@mail.ru>
2023-03-11 23:38:38 +1000

    Add tower

--- Assets/Scripts/classes/Building.cs
+++ Assets/Scripts/classes/Building.cs
@@ -1,5 +1,23 @@
 // Базовый класс здания
+using UnityEngine;
 public class Building : Entity, IBuilding
 {
     public int Size { get; set; }
+
+    public void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+        print("Drag");
+    }
+    public void OnMouseDown()
+    {
+        print("down");
+    }
+    private void Update()
+    {
+       
+    }
+
+  
+
 }
